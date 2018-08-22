0a3b851392699b4269f37909816c080efd378822
Bhavnasharma123 <40174161+Bhavnasharma123@users.noreply.github.com>
2018-08-22 21:51:55 +0530

    day20-code

---
+++ java/Arraysum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Arraysum{
+static int sum=0;
+public static void main(String[] args){
+
+int a[]={10,20,30,50};
+ 
+for(int i=0;i<a.length;i++)
+{
+sum=sum+a[i];
+}
+System.out.println(sum);
+
+}
+}
