08949b2d95356000d94e50a99939e0cc4303027f
Bhavnasharma123 <40174161+Bhavnasharma123@users.noreply.github.com>
2018-08-24 21:54:59 +0530

    dat22-code

---
+++ CBNST/GAUSS.C
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<process.h>
+#include<string.h>
+void main()
+{
+int n,i,j;
+float ax[10];
+float ay[10];
+float x;
+float nr,dr;
+float y=0;
+float h;
+float p;
+float diff[20][20] ;
+float y1,y2,y3,y4;
+clrscr();
+printf("\n enter the no. of terms-");
+scanf("%d",&n);
+printf("\n\n enter the value in the form of x-");
+for(i=0;i<n;i++)
+{
+printf("\n\n enter the of x %d- ",i+1);
+scanf("%f",&ax[i]);
+}
+printf("\n\n enter the value in the form y-");
+for(i=0;i<n;i++)
+{
+printf("\n\n enter the value of y %d-",i+1);
+scanf("%f",&ay[i]);
+}
+printf("\n enter the valueof x for");
+printf("\nwhich u want the value of y-");
+scanf("%f",&x);
+h=ax[1]-ax[0];
+for(i=0;i<n-1;i++)
+  {
+    diff[i][1]=ay[i+1]-ay[i];
+  }
+for(j=2;j<4;j++)
+  {
+  for(i=0;i<n-j;i++)
+     {
+       diff[i][j]=diff[i+1][j-1]-diff[i][j-1];
+     }
+  }
+  i=0;
+  do
+   {
+       i++;
+       }while(ax[i]<x);
+    i--;
+  p=(x-ax[i])/h;
+y1=p*diff[i][1];
+y2=p*(p-1)*diff[i-1][2]/2;
+y3=(p+1)*p*(p-1)*diff[i-2][3]/6;
+y4=(p+1)*p*(p-1)*(p-2)*diff[i-3][4]/24;
+y=ay[i]+y1+y2+y3+y4;
+printf("\nwhen x=%6.4f,y=%6.8f ",x,y);
+printf("\n\n\n pess enter to exit");
+getch();
+}
+
+
