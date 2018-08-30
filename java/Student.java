f8e78ddc77ce0c912c7d5e65c017e2e371edaca7
Bhavnasharma123 <40174161+Bhavnasharma123@users.noreply.github.com>
2018-08-30 23:38:11 +0530

    day24-code

---
+++ java/Student.java
@@ -0,0 +1,31 @@
+
+public class Student{
+int rollno;
+String name;
+static String  collname="KIET";
+
+
+
+public static void main(String[] args){
+
+Student s1 = new Student();
+s1.name="amit";
+s1.rollno=1;
+System.out.println(s1.name);
+System.out.println(s1.rollno);
+System.out.println(collname);
+
+Student s2 = new Student ();
+s2.name="Bhavna";
+s2.rollno=13;
+System.out.println(s2.name);
+System.out.println(s2.rollno);
+System.out.println(collname);
+
+
+
+}
+}
+ 
+
+
