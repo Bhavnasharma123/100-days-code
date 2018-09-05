b3978c3bc41d6d504cfe89afaaabf7cd22322bdf
Bhavnasharma123 <40174161+Bhavnasharma123@users.noreply.github.com>
2018-09-05 23:34:06 +0530

    day28-code

---
+++ java/Base.java
@@ -0,0 +1,28 @@
+class Base{
+private static String  m1(String msg)
+{
+System.out.println(msg);
+return msg;
+}
+
+static String m =m1("1");
+
+{
+m=m1("2");
+}
+
+ static 
+{
+m = m1("3");
+}
+
+public static void main(String[] args){
+Base o= new Base();
+}
+}
+
+
+
+
+
+
